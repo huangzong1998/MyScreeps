0481e5e2e9c3bb93c22eef058f08562b15660914
huangzong1998 <916112430@qq.com>
2020-02-01 12:49:40 +0800

    first

---
+++ default/role.builder.js
@@ -0,0 +1,33 @@
+var roleBuilder = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+		//creep处于build状态并且库存为空，则应该停止build去采矿
+	    if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.building = false;
+            creep.say('🔄 harvest');
+		}
+		//creep未处于build状态且库存满，则应该去build
+	    if(!creep.memory.building && creep.store.getFreeCapacity() == 0) {
+	        creep.memory.building = true;
+	        creep.say('🚧 build');
+	    }
+
+	    if(creep.memory.building) {
+	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+            if(targets.length) {
+                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+	    }
+	    else {
+	        var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+	    }
+	}
+};
+
+module.exports = roleBuilder;
---
+++ default/role.upgrader.js
@@ -0,0 +1,29 @@
+var roleUpgrader = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+
+        if(creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {//在升级状态并且空库存
+            creep.memory.upgrading = false;
+            creep.say('🔄 harvest');
+	    }
+	    if(!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {//不在升级并且满库存
+	        creep.memory.upgrading = true;
+	        creep.say('⚡ upgrade');
+	    }
+
+	    if(creep.memory.upgrading) {//升级
+            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+            }
+        }
+        else {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        }
+	}
+};
+
+module.exports = roleUpgrader;

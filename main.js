3d284c1e29bc00ec7a70eb56b15bd73985b23131
huangzong1998 <916112430@qq.com>
2020-02-01 13:05:50 +0800

    aaa

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
 var roleWorker = require('role.worker');
 
 
@@ -14,34 +11,6 @@ module.exports.loop = function () {
             delete Memory.creeps[name];
             //console.log('Clearing non-existing creep memory:', name);
         }
-    }
-
-	var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-	
-	if(builders.length < 1){
-		var newName = 'Builder' + Game.time;
-		//Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE,MOVE], newName, {memory: {role: 'builder'}});
-	}
-
-    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-    //console.log('Harvesters: ' + harvesters.length);
-
-    if(harvesters.length < 2) {
-        var newName = 'Harvester' + Game.time;
-        //console.log('Spawning new harvester: ' + newName);
-        //Game.spawns['Spawn1'].spawnCreep([WORK,WORK,CARRY,CARRY,MOVE,MOVE,MOVE], newName, 
-           // {memory: {role: 'harvester'}});
-    }
-    
-    var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-    //console.log('Upgraders: ' + upgraders.length);
-    
-    if(upgraders.length < 2) {
-        var newName = 'Upgrader' + Game.time;
-        //console.log('Spawning new upgrader: ' + newName);
-		//成本WORK100 CARRY50 MOVE50 ATTACK80
-        //Game.spawns['Spawn1'].spawnCreep([WORK,WORK,CARRY,CARRY,MOVE,MOVE,MOVE], newName, 
-           // {memory: {role: 'upgrader'}});
     }
 
     var workers = _.filter(Game.creeps, (creep) => creep.memory.role == 'worker');
@@ -64,18 +33,6 @@ module.exports.loop = function () {
 	//遍历所有creep
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
-		//建筑
-		if(creep.memory.role == 'builder') {
-			roleUpgrader.run(creep);
-		}
-		//采矿
-        if(creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
-        }
-		//升级
-        if(creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
-        }
         if(creep.memory.role == 'worker'){
             roleWorker.run(creep);
         }
